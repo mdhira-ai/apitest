7aa8d7249b34aa32c69e1c57dab966ac4363d1d0
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2022-12-04 15:59:39 -0500

    update msg

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
+    res.json({ message: "Welcome to ratemebro.com api." });
 })
 
 // Create a new Tutorial
